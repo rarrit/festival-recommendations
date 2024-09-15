d95a23b6f07f4032be15bf558a531ec7eaf588aa
KangSujin <tnwls7138@gmail.com>
2024-09-15 15:29:14 +0900

    메인 페이지 작업중

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,18 +3,18 @@ import styled from "styled-components";
 
 const Main = () => {
   useEffect(() => {
-    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
+    var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+        level: 3, // 지도의 확대 레벨
+      };
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    var map = new kakao.maps.Map(mapContainer, mapOption); 
-  }, [])
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+  }, []);
   return (
     <StMain id="main">
-      <div id="map" style={{width:"500px" ,height:"350px"}}></div>
+      <div id="map" style={{ width: "500px", height: "350px" }}></div>
     </StMain>
   );
 };
