30b3d5855267a49af2ecbf892a6a1c6dd2313205
KangSujin <tnwls7138@gmail.com>
2024-09-22 02:42:17 +0900

    update : 북마크 delete기능 구현 완료, 북마크 기능들 TanstackQuery로 리팩토링 완료

--- src/components/BookmarkButton.jsx
+++ src/components/BookmarkButton.jsx
@@ -1,10 +1,11 @@
 import useAuthStore from "@/core/store/authStore";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
 
 export function BookmarkButton({ festival, bookmarkList }) {
-  const [bookmarkOn, setBookmarkOn] = useState(false);
   const { isLoggedIn, userId } = useAuthStore();
+  const queryClient = useQueryClient();
 
   const isBookmarked = bookmarkList.some((item) => {
     return festival.fstvlNm === item.fstvlNm && userId === item.userId;
@@ -13,27 +14,45 @@ export function BookmarkButton({ festival, bookmarkList }) {
   console.log(isBookmarked);
 
   if (isLoggedIn) {
-    const saveBookmark = async () => {
-      try {
-        const response = await axios.post("http://localhost:4000/bookmarkFestivalList", {
+    const { data, isPending, isError } = useQuery({
+      queryKey: ["bookmarkFestivalList"],
+      queryFn: () => axios.get("http://localhost:4000/bookmarkFestivalList")
+    });
+
+    const saveBookmark = useMutation({
+      mutationFn: () =>
+        axios.post("http://localhost:4000/bookmarkFestivalList", {
           ...festival,
-          userId
-        });
-        console.log("API Response :", response.data);
-        if (response.data.result === "SUCCESS") {
-          setBookmarkOn(!bookmarkOn);
-        }
-      } catch (error) {
-        console.log("북마크 저장 실패");
+          userId,
+          id: `${festival.fstvlNm}${userId}`
+        }),
+      onSuccess: () => {
+        queryClient.invalidateQueries("bookmarkFestivalList");
       }
-    };
+    });
+
+    const deleteBookmark = useMutation({
+      mutationFn: () => axios.delete(`http://localhost:4000/bookmarkFestivalList/${festival.fstvlNm}${userId}`),
+      onSuccess: () => {
+        queryClient.invalidateQueries("bookmarkFestivalList");
+      }
+    });
+    console.log(festival.id);
+
+    if (isPending) {
+      return <div>로딩중입니다...</div>;
+    }
+
+    if (isError) {
+      return <div>데이터 조회 중 오류가 발생했습니다.</div>;
+    }
 
     return (
       <>
         {isBookmarked ? (
-          <button onClick={() => saveBookmark(festival)}>취소하기</button>
+          <button onClick={() => deleteBookmark.mutate(festival)}>취소하기</button>
         ) : (
-          <button onClick={() => saveBookmark(festival)}>저장하기</button>
+          <button onClick={() => saveBookmark.mutate(festival)}>저장하기</button>
         )}
       </>
     );
