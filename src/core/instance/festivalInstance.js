52cbf96c46f9e120889a9a89bfb6d6090bb25084
김민규 <tjfvndfud@gmail.com>
2024-09-22 23:22:16 +0900

    fix : kakao map api 수정

--- src/core/instance/festivalInstance.js
+++ src/core/instance/festivalInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 console.log(
-  `https://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api?serviceKey=${import.meta.env.VITE_KAKAO_MAP_KEY}&pageNo=1&numOfRows=100&type=JSON`
+  `http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api?serviceKey=${import.meta.env.VITE_KAKAO_MAP_KEY}&pageNo=1&numOfRows=100&type=JSON`
 );
 const festivalIns = axios.get(
-  `https://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api?serviceKey=EAmfJivTLtIuFxBdgR718mbgrR%2BN3XR4h3PqrUjDyKVBhrj3Y%2FxGRE4vUicjWvf00JOirrM8pE4JZGHVCP33IQ%3D%3D&pageNo=1&numOfRows=100&type=JSON`,
+  `http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api?serviceKey=EAmfJivTLtIuFxBdgR718mbgrR%2BN3XR4h3PqrUjDyKVBhrj3Y%2FxGRE4vUicjWvf00JOirrM8pE4JZGHVCP33IQ%3D%3D&pageNo=1&numOfRows=100&type=JSON`,
   {}
 );
 console.log("api=>", API_KEY);
